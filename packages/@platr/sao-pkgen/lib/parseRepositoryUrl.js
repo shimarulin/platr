e003bc215c5be0419a0707842f6678eb585662c1
Vyacheslav Shimarulin <shimarulin@gmail.com>
2020-03-16 19:46:09 +0300

    feat(package-templates): parse repository url

---
+++ packages/@platr/sao-pkgen/lib/parseRepositoryUrl.js
@@ -0,0 +1,62 @@
+const URL_TYPES = {
+  SSH: 'ssh',
+  HTTPS: 'https',
+  LINK: 'link',
+  UNKNOWN: 'unknown',
+}
+
+const getUrlType = (url) => (typeof url === 'string' || undefined) && ((url.search('git@') !== -1 && URL_TYPES.SSH) ||
+  (url.search(/\.git$/) !== -1 && URL_TYPES.HTTPS) ||
+  (url.search(/^https/) !== -1 && URL_TYPES.LINK) ||
+  URL_TYPES.UNKNOWN)
+
+const getHostFromUrl = (url) => {
+  if (typeof url !== 'string') {
+    return
+  }
+  const urlType = getUrlType(url)
+  switch (urlType) {
+    case URL_TYPES.SSH:
+      return (/(?:@)([-_a-zA-Z0-9.]+)/).exec(url)[1]
+    case URL_TYPES.HTTPS:
+      return (/(?:\/\/)([-_a-zA-Z0-9.]+)/).exec(url)[1]
+    case URL_TYPES.LINK:
+      return (/(?:\/\/)([-_a-zA-Z0-9.]+)/).exec(url)[1]
+    default:
+      return (/([-_a-zA-Z0-9.]+)/).exec(url)[1]
+  }
+}
+
+const getUserFromUrl = (url) => {
+  if (typeof url !== 'string') {
+    return
+  }
+  const urlHost = getHostFromUrl(url)
+  const re = new RegExp(`${urlHost}[/:]([-_a-zA-Z0-9]+)`)
+  return re.exec(url)[1]
+}
+
+const getRepoNameFromUrl = (url) => {
+  if (typeof url !== 'string') {
+    return
+  }
+  const urlUser = getUserFromUrl(url)
+  const re = new RegExp(`${urlUser}/([-_a-zA-Z0-9]+)`)
+  return re.exec(url)[1]
+}
+
+const parseRepositoryUrl = (url) => {
+  return {
+    host: getHostFromUrl(url),
+    owner: getUserFromUrl(url),
+    project: getRepoNameFromUrl(url),
+  }
+}
+
+module.exports = {
+  getUrlType,
+  getHostFromUrl,
+  getUserFromUrl,
+  getRepoNameFromUrl,
+  parseRepositoryUrl,
+}
---
+++ packages/@platr/sao-pkgen/test/parse-repository-url.test.js
@@ -0,0 +1,67 @@
+const { getUrlType, getHostFromUrl, getUserFromUrl, getRepoNameFromUrl } = require('../lib/parseRepositoryUrl')
+
+describe('Get type of repository URL', () => {
+  test('URL isn\'t a string', async () => {
+    expect(getUrlType(undefined)).toBeUndefined()
+  })
+  test('Git over SSH', async () => {
+    expect(getUrlType('git@github.com:username/package.git')).toEqual('ssh')
+  })
+  test('Git over HTTPS', async () => {
+    expect(getUrlType('git+https://github.com/username/package.git')).toEqual('https')
+  })
+  test('Git over HTTPS without prefix', async () => {
+    expect(getUrlType('https://github.com/username/package.git')).toEqual('https')
+  })
+  test('repository link', async () => {
+    expect(getUrlType('https://github.com/username/package')).toEqual('link')
+  })
+  test('unknown repository link', async () => {
+    expect(getUrlType('github.com/username/package')).toEqual('unknown')
+  })
+})
+
+describe('Get host of repository URL', () => {
+  test('Git over SSH', async () => {
+    expect(getHostFromUrl('git@github.com:username/package.git')).toEqual('github.com')
+  })
+  test('Git over HTTPS', async () => {
+    expect(getHostFromUrl('git+https://github.com/username/package.git')).toEqual('github.com')
+  })
+  test('repository link', async () => {
+    expect(getHostFromUrl('https://github.com/username/package')).toEqual('github.com')
+  })
+  test('unknown repository link', async () => {
+    expect(getHostFromUrl('github.com/username/package')).toEqual('github.com')
+  })
+})
+
+describe('Get user from repository URL', () => {
+  test('Git over SSH', async () => {
+    expect(getUserFromUrl('git@github.com:username/package.git')).toEqual('username')
+  })
+  test('Git over HTTPS', async () => {
+    expect(getUserFromUrl('git+https://github.com/username/package.git')).toEqual('username')
+  })
+  test('repository link', async () => {
+    expect(getUserFromUrl('https://github.com/username/package')).toEqual('username')
+  })
+  test('unknown repository link', async () => {
+    expect(getUserFromUrl('github.com/username/package')).toEqual('username')
+  })
+})
+
+describe('Get repository name from repository URL', () => {
+  test('Git over SSH', async () => {
+    expect(getRepoNameFromUrl('git@github.com:username/package.git')).toEqual('package')
+  })
+  test('Git over HTTPS', async () => {
+    expect(getRepoNameFromUrl('git+https://github.com/username/package.git')).toEqual('package')
+  })
+  test('repository link', async () => {
+    expect(getRepoNameFromUrl('https://github.com/username/package')).toEqual('package')
+  })
+  test('unknown repository link', async () => {
+    expect(getRepoNameFromUrl('github.com/username/package')).toEqual('package')
+  })
+})
